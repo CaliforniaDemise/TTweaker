50cfcb66633fb7351bd30fb79b5f0e8ecde6dd0e
CaliforniaDemise <vwwwwv@proton.me>
2024-09-13 21:09:41 +0300

    Pizzacraft Mortar support.

--- src/main/java/surreal/ttweaker/crafttweaker/integrations/pizzacraft/Mortar.java
+++ src/main/java/surreal/ttweaker/crafttweaker/integrations/pizzacraft/Mortar.java
@@ -1,8 +1,7 @@
 package surreal.ttweaker.crafttweaker.integrations.pizzacraft;
 
-import com.tiviacz.pizzacraft.crafting.bakeware.BaseShapelessOreRecipe;
-import com.tiviacz.pizzacraft.crafting.bakeware.IBakewareRecipe;
-import com.tiviacz.pizzacraft.crafting.bakeware.PizzaCraftingManager;
+import com.tiviacz.pizzacraft.crafting.mortar.IMortarRecipe;
+import com.tiviacz.pizzacraft.crafting.mortar.MortarRecipeManager;
 import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ModOnly;
 import crafttweaker.annotations.ZenRegister;
@@ -16,11 +15,11 @@ import surreal.ttweaker.utils.CTUtils;
 
 @ZenRegister
 @ModOnly("pizzacraft")
-@ZenClass("mods.pizzacraft.Bakeware")
-public class Bakeware {
+@ZenClass("mods.pizzacraft.Mortar")
+public class Mortar {
 
     @ZenMethod
-    public static void addRecipe(IItemStack output, IIngredient[] inputs) {
+    public static void addRecipe(IItemStack output, int duration, IIngredient[] inputs) {
         if (inputs.length == 0) {
             CraftTweakerAPI.getLogger().logError("Insufficient amount of inputs!");
             return;
@@ -29,30 +28,28 @@ public class Bakeware {
             CraftTweakerAPI.getLogger().logError("Amount of inputs can only be between 1 and 9!");
             return;
         }
-        PizzaCraftingManager manager = getManager();
-        manager.addRecipe(new BaseShapelessOreRecipe(CraftTweakerMC.getItemStack(output), CTUtils.getObjects(inputs)));
+        MortarRecipeManager manager = getManager();
+        manager.addShapelessRecipe(CraftTweakerMC.getItemStack(output), duration, CTUtils.getObjects(inputs));
     }
 
     @ZenMethod
     public static void remove(IItemStack output) {
-        PizzaCraftingManager manager = getManager();
+        MortarRecipeManager manager = getManager();
         ItemStack out = CraftTweakerMC.getItemStack(output);
-        for (IBakewareRecipe recipe : manager.getRecipeList()) {
+        for (IMortarRecipe recipe : manager.getRecipeList()) {
             if (ItemStack.areItemStacksEqual(recipe.getRecipeOutput(), out)) {
                 manager.removeRecipe(recipe);
                 return;
             }
         }
-        CraftTweakerAPI.getLogger().logError("Could not find recipe with output " + output.getDisplayName());
     }
 
     @ZenMethod
     public static void removeAll() {
-        PizzaCraftingManager manager = PizzaCraftingManager.getPizzaCraftingInstance();
-        manager.getRecipeList().clear();
+        getManager().getRecipeList().clear();
     }
 
-    private static PizzaCraftingManager getManager() {
-        return PizzaCraftingManager.getPizzaCraftingInstance();
+    private static MortarRecipeManager getManager() {
+        return MortarRecipeManager.getMortarManagerInstance();
     }
 }
