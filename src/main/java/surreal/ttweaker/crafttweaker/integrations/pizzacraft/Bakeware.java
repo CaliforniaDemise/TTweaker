c29147c45e93a0037d105628a14984225fa91a0f
CaliforniaDemise <vwwwwv@proton.me>
2024-09-13 21:00:33 +0300

    Pizzacraft Bakeware support.

---
+++ src/main/java/surreal/ttweaker/crafttweaker/integrations/pizzacraft/Bakeware.java
@@ -0,0 +1,59 @@
+package surreal.ttweaker.crafttweaker.integrations.pizzacraft;
+
+import com.tiviacz.pizzacraft.crafting.bakeware.BaseShapelessOreRecipe;
+import com.tiviacz.pizzacraft.crafting.bakeware.IBakewareRecipe;
+import com.tiviacz.pizzacraft.crafting.bakeware.PizzaCraftingManager;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IIngredient;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+import surreal.ttweaker.utils.CTUtils;
+
+@ZenRegister
+@ModOnly("pizzacraft")
+@ZenClass("mods.pizzacraft.Bakeware")
+public class Bakeware {
+
+    @ZenMethod
+    public static void addRecipe(IItemStack output, IIngredient[] inputs) {
+        if (inputs.length == 0) {
+            CraftTweakerAPI.getLogger().logError("Insufficient amount of inputs!");
+            return;
+        }
+        else if (inputs.length > 9) {
+            CraftTweakerAPI.getLogger().logError("Amount of inputs can only be between 1 and 9!");
+            return;
+        }
+        PizzaCraftingManager manager = getManager();
+        manager.addRecipe(new BaseShapelessOreRecipe(CraftTweakerMC.getItemStack(output), CTUtils.getObjects(inputs)));
+    }
+
+    @ZenMethod
+    public static void remove(IItemStack output) {
+        PizzaCraftingManager manager = getManager();
+        for (IBakewareRecipe recipe : manager.getRecipeList()) {
+            if (ItemStack.areItemStacksEqual(recipe.getRecipeOutput(), CraftTweakerMC.getItemStack(output))) {
+                manager.removeRecipe(recipe);
+                return;
+            }
+        }
+        CraftTweakerAPI.getLogger().logError("Could not find recipe with output " + output.getDisplayName());
+    }
+
+    @ZenMethod
+    public static void removeAll() {
+        PizzaCraftingManager manager = PizzaCraftingManager.getPizzaCraftingInstance();
+        for (IBakewareRecipe recipe : manager.getRecipeList()) {
+            manager.removeRecipe(recipe);
+        }
+    }
+
+    private static PizzaCraftingManager getManager() {
+        return PizzaCraftingManager.getPizzaCraftingInstance();
+    }
+}
--- src/main/java/surreal/ttweaker/utils/CTUtils.java
+++ src/main/java/surreal/ttweaker/utils/CTUtils.java
@@ -1,12 +1,35 @@
 package surreal.ttweaker.utils;
 
+import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.item.IItemDefinition;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.oredict.IOreDictEntry;
 import crafttweaker.mc1120.item.MCItemDefinition;
 import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
 
 public class CTUtils {
 
     public static IItemDefinition getItem(Item item) {
         return new MCItemDefinition(item.getRegistryName().toString(), item);
+    }
+
+    public static Ingredient[] getIngredients(IIngredient[] ingredients) {
+        Ingredient[] ings = new Ingredient[ingredients.length];
+        for (int i = 0; i < ingredients.length; i++) {
+            ings[i] = CraftTweakerMC.getIngredient(ingredients[i]);
+        }
+        return ings;
+    }
+
+    public static Object[] getObjects(IIngredient[] ingredients) {
+        Object[] inputs = new Object[ingredients.length];
+        for (int i = 0; i < ingredients.length; i++) {
+            IIngredient ing = ingredients[i];
+            if (ing instanceof IOreDictEntry) inputs[i] = ((IOreDictEntry) ing).getName();
+            else inputs[i] = CraftTweakerMC.getItemStack(ing);
+        }
+        return inputs;
     }
 }
